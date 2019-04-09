c4dd29d5ae43fa40f9959cb560402eeb8e7b1405
cristianbgp <cristian.granda.pastor@gmail.com>
2019-04-09 16:26:31 -0500

    Add titleize to the last names

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,24 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8 }, confirmation: true, format: { with: /.*[0-9].*/ }
   validates :password_confirmation, presence:true
   validates :password_hint, length: { maximum: 100 }
+  before_save :capitalize_first_name
+  before_save :titleize_last_name
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def capitalize_first_name
+    self.first_name.capitalize!
+  end
+
+  def titleize_last_name
+    self.last_name = self.last_name.split.map do |word|
+      if ["del", "la", "de"].include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end.join(" ")
   end
 end
