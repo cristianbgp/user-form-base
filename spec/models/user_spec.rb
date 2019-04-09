dbb1022ddc64cf50ed43df076e6e6ebe0d95506a
cristianbgp <cristian.granda.pastor@gmail.com>
2019-04-09 17:22:31 -0500

    Add some basic test to the model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject { described_class.new(
+    first_name: "Cristian",
+    last_name: "Granda",
+    username: "cristianbgp",
+    email: "cristian.granda.pastor@gmail.com",
+    email_confirmation: "cristian.granda.pastor@gmail.com",
+    password: "12345678",
+    password_confirmation: "12345678",
+    password_hint: "1234"
+  ) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a username" do
+    subject.username = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
