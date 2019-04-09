8185b65baf21acdb9ed3f15b2f5dc08ade4ac9bc
cristianbgp <cristian.granda.pastor@gmail.com>
2019-04-09 13:26:12 -0500

    Add validations to the model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { in: 2..60 }
+  validates :last_name, presence: true, length: { in: 2..60 }
+  validates :username, presence: true, uniqueness: true , length: { in: 2..20 }
+  validates :email, confirmation: true, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :email_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 8 }, confirmation: true
+  validates :password_confirmation, presence:true
+  validates :password_hint, length: { maximum: 100 }
+
   def full_name
     "#{first_name} #{last_name}"
   end
